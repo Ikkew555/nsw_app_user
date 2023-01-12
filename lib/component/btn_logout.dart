cfce3cf9c22e204efd665871df691388e9872479
ikkew <nathakorn@agiletech.dev>
2023-01-12 13:44:55 +0700

    logout_navigate

--- lib/component/btn_logout.dart
+++ lib/component/btn_logout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nsw_app/config.dart';
+import 'package:nsw_app/pages/login/login.view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LogOutButton extends StatefulWidget {
   const LogOutButton({Key? key}) : super(key: key);
@@ -9,6 +11,15 @@ class LogOutButton extends StatefulWidget {
 }
 
 class _LogOutButtonState extends State<LogOutButton> {
+  // final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  // Future<void> _logout() async {
+  //   final SharedPreferences prefs = await _prefs;
+
+  //   final username_out = await prefs.remove('username');
+  //   final password_out = await prefs.remove('password');
+  // }
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
@@ -22,7 +33,17 @@ class _LogOutButtonState extends State<LogOutButton> {
         primary: Colors.white,
         elevation: 5,
       ),
-      onPressed: () {},
+      onPressed: () {
+        // _logout();
+        // Conf.instance.clear();
+        // User.instance.setname("");
+        // User.instance.setsurname("");
+        // User.instance.setuid("");
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const LoginPage()),
+            (route) => false);
+      },
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
